3065a4212b3c428400b7bd324ff9e5dd834489ab
Emilio Barrera <barrera_emilio@yahoo.es>
2024-10-07 16:06:20 +0100

    Add project files.

---
+++ CRUDWithRepository/DAL/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDWithRepository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDWithRepository.DAL
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ CRUDWithRepository/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDWithRepository.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
